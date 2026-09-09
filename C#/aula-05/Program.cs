52c99a4eb38d36fa6cde0e7a98b4bee7ce5ff035
Augusto Cesar <augustocesaardev@gmail.com>
2026-09-09 09:48:55 -0300

    Usando json
    Aprendendo com usar json com o C#

---
+++ C#/aula-05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Aula
+{
+    public class Program
+    {
+        public class Produto
+        {
+            public string NomeProduto { get; set; } = "";
+            public double PrecoProduto { get; set; } 
+            public int EstoqueProduto { get; set; } 
+            public int CodigoProduto { get; set; }
+        }
+        public class ProdutoResponse
+        {
+            public List <Produto> produtos { get; set; } = new();
+        }
+
+        public static void Main()
+        {
+            string json = File.ReadAllText("Produtos.json");
+        
+            ProdutoResponse? dados = 
+                JsonSerializer.Deserialize<ProdutoResponse>(json);
+
+            foreach(Produto produto in dados.produtos)
+            {
+                Console.WriteLine("Nome Produto: " + produto.NomeProduto);
+                Console.WriteLine("Preço: R$" + produto.PrecoProduto);
+                Console.WriteLine("Estoque: " + produto.EstoqueProduto);
+                Console.WriteLine("Codigo: " + produto.CodigoProduto);
+                Console.WriteLine("---------------------------------");
+            }
+        }
+    }
+}
